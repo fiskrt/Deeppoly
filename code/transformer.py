394dc2aab62ff1537d9910da6e365235fee90d11
fiskrt <filipskogh99@hotmail.com>
2022-11-06 01:39:57 +0100

    abstract affine transformer

--- code/transformer.py
+++ code/transformer.py
@@ -14,22 +14,33 @@ class AbstractLayer(nn.Module):
 
 class AffineLayer(nn.Module):
 
-    def __init__(self):
+    def __init__(self, layer):
         super().__init__()
         # Each node contain a upper and lower bound: 1D array
         self.ub = None
         self.lb = None
         # Each node contain a upper and lower linear constraint:
-        self.uc = None
-        self.lc = None
+        self.w = layer.weight 
+        self.b = layer.bias
         
     def forward(self, prev_layer):
         # Push shape from previous layer through this layer
 
+        # For each node in this layer, we have a constraint
+        # with 'in_features'+1 coefficients.
+        # The coefficient matrix is simply w and b concatinated.
+
+        w_pos = self.w >= 0.
+        self.ub = (w_pos*self.w)@prev_layer.ub+(~(w_pos)*self.w)@prev_layer.lb+self.b 
+        self.lb = (w_pos*self.w)@prev_layer.lb+(~(w_pos)*self.w)@prev_layer.ub+self.b 
+
+
+
 
 class InputLayer(nn.Module):
     """
-        Clamp stuff
+        Input layer have no previous layer, but the bounds are given.
+        We only have to clamp these to satisify normalization.
     """
     pass
 
